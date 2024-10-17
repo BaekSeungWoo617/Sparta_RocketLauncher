6dc430ddba58e6de1322666cd9371f45e2a91559
BaekSeungWoo617 <koke100@naver.com>
2024-10-17 13:08:32 +0900

    AddBar

--- Rocket_Launcher_Standard/Assets/Scripts/Rocket.cs
+++ Rocket_Launcher_Standard/Assets/Scripts/Rocket.cs
@@ -5,24 +5,34 @@ public class Rocket : MonoBehaviour
 {
     private Rigidbody2D _rb2d;
     [SerializeField]
-    private float fuel = 100f;
+    private float fuel = 400f;
     Rigidbody2D RocketRigidbody2D;
-
+    RectTransform FilledTransform;
     private readonly float SPEED = 5f;
     private readonly float FUELPERSHOOT = 10f;
     [SerializeField] private TextMeshProUGUI currentScoreTxt;
     [SerializeField] private TextMeshProUGUI HighScoreTxt;
     float CurrentScore;
     float HighScore;
-
+    float fillamount;
 
     void Awake()
     {
         // TODO : Rigidbody2D 컴포넌트를 가져옴(캐싱) 
         RocketRigidbody2D = GetComponent<Rigidbody2D>();
+        FilledTransform = GameObject.Find("Filled").GetComponent<RectTransform>();
     }
+    private void Update()
+    {
+        ScoreUpdate();
+    }
+    public void FilledImage()
+    {
+        Vector2 vector2Transform = FilledTransform.sizeDelta;
+        vector2Transform.x = fuel;
+        FilledTransform.sizeDelta = vector2Transform;
 
-
+    }
     public void Shoot()
     {
         // TODO : fuel이 넉넉하면 윗 방향으로 SPEED만큼의 힘으로 점프, 모자라면 무시
@@ -31,6 +41,7 @@ public class Rocket : MonoBehaviour
             Vector2 rocketForce = new Vector2(0f, SPEED);
             RocketRigidbody2D.AddForce(rocketForce, ForceMode2D.Impulse);
                 fuel -= FUELPERSHOOT;
+
         }
     }
 
@@ -39,14 +50,14 @@ public class Rocket : MonoBehaviour
     {
         SceneManager.LoadScene("RocketLauncher");
     }
-    private void Update()
+    public void ScoreUpdate()
     {
         CurrentScore = this.gameObject.transform.position.y;
         ScoreUIUpdate(currentScoreTxt, CurrentScore);
         HighScoreDataUpdate();
         ScoreUIUpdate(HighScoreTxt, HighScore);
+        FilledImage();
     }
-
 
     void ScoreUIUpdate(TextMeshProUGUI textUI,float score)
     {
